37202a1a89bbf86eefa4da160c5667bdbf748ee6
Seth Bashford <sbash64@gmail.com>
2020-02-29 11:52:55 -0600

    first test

--- test/tbd.cpp
+++ test/tbd.cpp
@@ -1,1 +1,33 @@
-int main() {}
+#include <string>
+#include <vector>
+#include <functional>
+#include <ostream>
+
+namespace testcpp {
+void test(const std::vector<std::function<void()>> &, std::ostream &stream) {
+    stream << "pass\n";
+}
+
+void assertEqual(const std::string &expected, const std::string &actual) {}
+}
+
+#include <sstream>
+#include <iostream>
+
+namespace testcpp {
+namespace {
+void passes() {}
+
+void testPassedOnlyTestShowsPassedMessage() {
+    std::stringstream stream;
+    test({passes}, stream);
+    assertEqual("pass\n", stream.str());
+}
+}
+}
+
+namespace testcpp {
+void main() { test({testPassedOnlyTestShowsPassedMessage}, std::cout); }
+}
+
+int main() { testcpp::main(); }
