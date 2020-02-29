cd9afb170386fd9b9bcf95d423e0140dd2ec3804
Seth Bashford <sbash64@gmail.com>
2020-02-29 11:59:10 -0600

    crazy

--- test/tbd.cpp
+++ test/tbd.cpp
@@ -4,11 +4,22 @@
 #include <ostream>
 
 namespace testcpp {
-void test(const std::vector<std::function<void()>> &, std::ostream &stream) {
-    stream << "pass\n";
+int failed_;
+
+void failed() { ++failed_; }
+
+void test(
+    const std::vector<std::function<void()>> &tests, std::ostream &stream) {
+    for (auto f : tests)
+        f();
+    stream << (failed_ == 0 ? "pass\n" : "fail fails\n");
+    failed_ = 0;
 }
 
-void assertEqual(const std::string &expected, const std::string &actual) {}
+void assertEqual(const std::string &expected, const std::string &actual) {
+    if (expected != actual)
+        failed();
+}
 }
 
 #include <sstream>
@@ -18,16 +29,28 @@ namespace testcpp {
 namespace {
 void passes() {}
 
+void fails() { assertEqual("a", "b"); }
+
 void testPassedOnlyTestShowsPassedMessage() {
     std::stringstream stream;
     test({passes}, stream);
     assertEqual("pass\n", stream.str());
 }
+
+void testFailedOnlyTestShowsFailedMessage() {
+    std::stringstream stream;
+    test({fails}, stream);
+    assertEqual("fail fails\n", stream.str());
+}
 }
 }
 
 namespace testcpp {
-void main() { test({testPassedOnlyTestShowsPassedMessage}, std::cout); }
+void main() {
+    test({testPassedOnlyTestShowsPassedMessage,
+             testFailedOnlyTestShowsFailedMessage},
+        std::cout);
+}
 }
 
 int main() { testcpp::main(); }
