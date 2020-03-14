e4fa31d414bd4601283dd51d325de9bcc91680cc
Seth Bashford <sbash64@gmail.com>
2020-03-14 13:25:53 -0500

    refactor

--- test/tbd.cpp
+++ test/tbd.cpp
@@ -4,15 +4,26 @@
 #include <ostream>
 
 namespace testcpp {
+struct Test {
+    std::function<void()> f;
+    std::string name;
+};
+
+std::string failedName_;
+std::string currentName_;
 int failed_;
 
-void failed() { ++failed_; }
+void failed() {
+    ++failed_;
+    failedName_ = currentName_;
+}
 
-void test(
-    const std::vector<std::function<void()>> &tests, std::ostream &stream) {
-    for (auto f : tests)
-        f();
-    stream << (failed_ == 0 ? "pass\n" : "fail fails\n");
+void test(const std::vector<Test> &tests, std::ostream &stream) {
+    for (auto test : tests) {
+        currentName_ = test.name;
+        test.f();
+    }
+    stream << (failed_ == 0 ? "pass\n" : "fail " + failedName_ + '\n');
     failed_ = 0;
 }
 
@@ -31,24 +42,30 @@ void passes() {}
 
 void fails() { assertEqual("a", "b"); }
 
+void assertEqual(const std::string &s, const std::stringstream &stream) {
+    testcpp::assertEqual(s, stream.str());
+}
+
 void testPassedOnlyTestShowsPassedMessage() {
     std::stringstream stream;
-    test({passes}, stream);
-    assertEqual("pass\n", stream.str());
+    test({{passes, "passes"}}, stream);
+    assertEqual("pass\n", stream);
 }
 
 void testFailedOnlyTestShowsFailedMessage() {
     std::stringstream stream;
-    test({fails}, stream);
-    assertEqual("fail fails\n", stream.str());
+    test({{fails, "fails"}}, stream);
+    assertEqual("fail fails\n", stream);
 }
 }
 }
 
 namespace testcpp {
 void main() {
-    test({testPassedOnlyTestShowsPassedMessage,
-             testFailedOnlyTestShowsFailedMessage},
+    test({{testPassedOnlyTestShowsPassedMessage,
+              "testPassedOnlyTestShowsPassedMessage"},
+             {testFailedOnlyTestShowsFailedMessage,
+                 "testFailedOnlyTestShowsFailedMessage"}},
         std::cout);
 }
 }
