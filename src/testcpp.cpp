a6709c82e53f87715277723a1b7a0718f3fa2f06
Seth Bashford <sbash64@gmail.com>
2020-04-04 10:52:02 -0500

    refactor

--- src/testcpp.cpp
+++ src/testcpp.cpp
@@ -1,27 +1,21 @@
 #include "testcpp.hpp"
 
 namespace testcpp {
-static const char *failedName;
-static const char *currentName;
 static bool failed_;
 
 static void failed() {
     failed_ = true;
-    failedName = currentName;
 }
 
 void test(const std::vector<Test> &tests, std::ostream &stream) {
     bool passed{true};
     for (const auto &test : tests) {
-        const auto restoreName{currentName};
-        currentName = test.name.c_str();
         test.f();
         if (failed_) {
             passed = false;
-            stream << "fail " << failedName << '\n';
+            stream << "fail " << test.name.c_str() << '\n';
         }
         failed_ = false;
-        currentName = restoreName;
     }
     if (passed)
         stream << "pass\n";
